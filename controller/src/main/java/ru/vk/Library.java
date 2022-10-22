6c283b73f4ec0804c957ae9c1993ea8ccaeef741
Alena Bushkina <aa-bushkina@mail.ru>
2022-10-22 21:16:47 +0300

    delete non-usable methods in Library class

--- controller/src/main/java/ru/vk/Library.java
+++ controller/src/main/java/ru/vk/Library.java
@@ -9,18 +9,4 @@ import java.util.List;
 public final class Library
 {
   private List<Book> books;
-
-  public void addBook(final @NotNull Book book)
-  {
-    if (!books.contains(book))
-    {
-      books.add(book);
-    }
-  }
-
-  public void deleteBook(final @NotNull Book book)
-  {
-    books.remove(book);
-  }
-
 }
