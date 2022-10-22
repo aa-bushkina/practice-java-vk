6eb6986b3e076bd7aea227050c7143129fe5c3ca
Alena Bushkina <aa-bushkina@mail.ru>
2022-10-22 23:36:25 +0300

    add @Nullable notation

--- models/src/main/java/ru/vk/Author.java
+++ models/src/main/java/ru/vk/Author.java
@@ -3,6 +3,7 @@ package ru.vk;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.Value;
+import org.jetbrains.annotations.Nullable;
 
 @Value
 @AllArgsConstructor
@@ -10,9 +11,10 @@ public class Author
 {
   @NonNull
   final String surname;
+  @Nullable
   final String firstname;
+  @Nullable
   final String patronymic;
-
   @NonNull
   final String country;
 }
