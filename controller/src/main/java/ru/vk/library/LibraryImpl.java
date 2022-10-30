0de0265eb1d6b1f46ffa6502db66951d2e2da3c5
Alena Bushkina <aa-bushkina@mail.ru>
2022-10-30 14:06:51 +0300

    make adding Book less than O(n)

--- controller/src/main/java/ru/vk/library/LibraryImpl.java
+++ controller/src/main/java/ru/vk/library/LibraryImpl.java
@@ -12,14 +12,16 @@ import ru.vk.books.BooksFactory;
 import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 @Getter
 public final class LibraryImpl implements Library
 {
   private List<Book> books;
-  private final int capacity;
   @NotNull
   private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+  private TreeSet<Integer> emptyIndexes;
 
   @AssistedInject
   LibraryImpl(@NotNull @Named("file") BooksFactory factory,
@@ -27,18 +29,22 @@ public final class LibraryImpl implements Library
               @Assisted @NotNull final String filePath)
   {
     final ArrayList<Book> tmpBooks = (ArrayList<Book>) factory.books(filePath);
-    this.capacity = capacity;
     books = new ArrayList<>(capacity);
     if (tmpBooks.size() > capacity)
     {
       throw new RuntimeException("The allowed container size has been exceeded");
     }
     books = tmpBooks;
+    emptyIndexes = new TreeSet<>();
+    for (int i = tmpBooks.size(); i < books.size(); i++)
+    {
+      emptyIndexes.add(i);
+    }
   }
 
   public Book takeBook(final int cellNum)
   {
-    if (cellNum > capacity || cellNum < 0)
+    if (cellNum > books.size() || cellNum < 0)
     {
       throw new RuntimeException("Invalid cell number");
     }
@@ -49,21 +55,19 @@ public final class LibraryImpl implements Library
     }
     Book tmpBook = books.get(cellNum);
     books.set(cellNum, null);
+    emptyIndexes.add(cellNum);
     printBook(tmpBook);
     return tmpBook;
   }
 
   public void addBook(@NotNull final Book book)
   {
-    for (int i = 0; i < books.size(); i++)
+    if (emptyIndexes.isEmpty())
     {
-      if (books.get(i) == null)
-      {
-        books.set(i, book);
-        return;
-      }
+      throw new RuntimeException("The library is full");
     }
-    throw new RuntimeException("The library is full");
+    books.add(emptyIndexes.first(), book);
+    emptyIndexes.remove(emptyIndexes.first());
   }
 
   public void printBook(@NotNull final Book book)
